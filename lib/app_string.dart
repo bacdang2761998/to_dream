d856ad1706fd52835c2c1396b199647c4da23ade
bacdv <bacdang2761998@gmail.com>
2021-11-15 18:06:48 +0700

    update project

---
+++ lib/app_string.dart
@@ -0,0 +1,9 @@
+class AppString {
+  static const String CONTENT1 =
+      " No one can make you feel inferior without your consent. — Eleanor Roosevelt";
+  static const String CONTENT2 =
+      "I am thankful to all who said no to me. It is because of them that I’m doing it myself. — Albert Einstein";
+  static const String CONTENT3 =
+      "Don’t stop when you are tired. Stop when you are done - Marilyn Monroe.";
+  static const String ACCEPT = "I have read and agree to the above terms";
+}
---
+++ lib/screen/webview_accep.dart
@@ -0,0 +1,33 @@
+import 'package:dream/app_url.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewAccep extends StatelessWidget {
+  const WebViewAccep({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 4,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          AppUrl.UrlAccep,
+          style: TextStyle(fontStyle: FontStyle.italic, fontSize: 16),
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.share))],
+      ),
+      body: WebView(
+        initialUrl: AppUrl.UrlAccep,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
